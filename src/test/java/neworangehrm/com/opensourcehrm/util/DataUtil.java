84dfe4a4807daff3f832c6de6ab412bf4be4fdd8
viswamprasad <viswamprasad@gmail.com>
2017-12-22 00:32:29 +0530

    testenv.properties and Code to read the properties file is been added

--- src/test/java/neworangehrm/com/opensourcehrm/util/DataUtil.java
+++ src/test/java/neworangehrm/com/opensourcehrm/util/DataUtil.java
@@ -1,7 +1,20 @@
 package neworangehrm.com.opensourcehrm.util;
 
-public class DataUtil {
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
-    public static String baseUrl = System.getProperty("baseUrl", "http://opensource.demo.orangehrmlive.com");
+public class DataUtil {
+    public static Properties readProperties = new Properties();
+
+    static {
+        try {
+            readProperties.load(new FileInputStream("src/test/resources/testenv.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String baseUrl = System.getProperty("baseUrl", readProperties.getProperty("baseurl"));
 
 }
